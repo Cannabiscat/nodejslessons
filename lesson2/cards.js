9b8169783893a02512fb43c07e69aac2612fb07a
AlexVjatkin <alexander.vjatkin@gmail.com>
2016-03-02 12:47:08 +0300

    Update cards.js

--- lesson2/cards.js
+++ lesson2/cards.js
@@ -1,20 +1,30 @@
+'use strict';
 var suits=['♤','♥','♧','♦'];
 var noms=['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
-var deck={};
+var deck=[];
 noms.forEach(function(nom) {
 	suits.forEach(function(suit) {
+		var val={};
 		if (nom=='Jack'||nom=='Queen'||nom=='King') {
-			deck[nom+suit]=10;
+			val[nom+suit]=10;
 		} else if(nom=='Ace') {
-			deck[nom+suit]=11;
+			val[nom+suit]=11;
 		} else {
-			deck[nom+suit]=nom;
+			val[nom+suit]=parseInt(nom);
 		};
+		deck.push(val);
 	});
 });
-console.log(Object.keys(deck).length);
 function shuffleDeck(deck) {
 	var temp;
 	var index;
-		
+	for (var i=0; i< deck.length; i++) {
+		index = Math.floor(Math.random()*(deck.length - i)) + i;
+		temp = deck[i];
+		deck[i] = deck[index];
+		deck[index] = temp;
+	};
 };
+console.log(deck);
+shuffleDeck(deck);
+console.log(deck);
