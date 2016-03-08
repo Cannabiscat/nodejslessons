ae7e540f338cf0ff6779a42f92140033033fe0a7
AlexVjatkin <alexander.vjatkin@gmail.com>
2016-03-08 19:21:31 +0300

    Not yet final update

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,36 +1,76 @@
 'use strict';
 
 //require('minimist');
-var readline = require('readline');
-var View = require('./view.js');
-var Model = require('./model.js');
+const readline = require('readline');
+const View = require('./view.js');
+const Model = require('./model.js');
 
 
-var deck = new Model.Deck();
-var player = new Model.Player();
-var comp = new Model.Player();
-var view = new View();
-deck.shuffleDeck();
+const deck = new Model.Deck();
+const player = new Model.Player();
+const comp = new Model.Player();
+const view = new View();
 
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+rl.setPrompt('One more card?(y/n)> ');
+view.start();
 
-view.show('Ход компьютера');
-while (comp.status) {
-    var nextC = deck.nextCard();
-    comp.addCard(nextC);
-    view.show(comp.getCards());
-    var compare = player.getSum() - comp.getSum();
-    if (comp.getSum() > 21) {
-        view.win();
-        comp.status = false;
-        break;
-    }
-    ;
-    if (compare < 0) {
-        view.lose();
-        comp.status = false;
-    }
-    ;
-}
-;
+rl.question('Are you ready to play a game?(y/n)', (answ) => {
+		if (answ === 'y') {
+			deck.shuffleDeck();
+			const next = deck.nextCard;
+			player.addCard(next());
+			view.show('Your card is: ' + player.getCards());
+			view.show('You have ' + player.getSum());
+			rl.prompt();
+			rl.on('line', (line) => {
 
-
+				if (line === 'n') {
+					rl.close();
+				} else if (line === 'y') {
+					player.addCard(next());
+					view.show(player.getCards());
+					view.show('You have ' + player.getSum());
+				} else {
+					view.show('Wrong answer, enter "y" or "n"!')
+				}
+				if (player.getSum() > 21) {
+					view.busted();
+					player.status = false;
+					rl.close();
+				} else {
+					rl.prompt();
+				}
+			}).on('close', function () {
+				if (player.status) {
+					view.show('Computer moving');
+					while (comp.status) {
+						comp.addCard(next());
+						view.show(comp.getCards());
+						let compare = player.getSum() - comp.getSum();
+						if (comp.getSum() > 21) {
+							view.win();
+							comp.status = false;
+							break;
+						}
+						if (compare < 0) {
+							view.lose();
+							comp.status = false;
+						}
+					}
+				} else {
+					view.lose();
+					rl.close();
+				}
+				process.exit(0);
+			});
+		}
+		else if (answ === 'n') {
+			view.show('Goodbye!');
+			rl.close();
+		}
+	}
+);
